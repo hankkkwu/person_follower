d0a37edf68fd22f636cb44710bce5ca52239eea5
hankkkwu <two30022001@hotmail.com>
2021-01-06 17:28:16 +0800

    Add PID controller for linear and angular velocity

--- src/cv_command.py
+++ src/cv_command.py
@@ -13,6 +13,7 @@
 import rospy
 import cv2
 import numpy as np
+import time
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -40,7 +41,14 @@ class CVControl:
 		self.image_sub = rospy.Subscriber("/decompressed_img", Image, self.img_callback)
 
         # Turtlebot command publisher
-		self.cmd_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=3)
+		self.cmd_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+
+		# The first parameter is then target: [linear target, angular target] = [area of bounding box, center of bounding box]
+		self.PID_controller = PID([150000, 320], [0.0000045, 0.002], [0, 0], [0.000001, 0.0005])
+
+		# Setup the max linear velocity and angular velocity
+		self.max_lin_vel = 0.5
+		self.max_ang_vel = 1.5
 
 
     # Main function that repeatedly gets called
@@ -50,7 +58,7 @@ class CVControl:
 		# initialize the list of class labels MobileNet SSD was trained to
 		# detect, then generate a set of bounding box colors for each class
 		"""
-		CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+		CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",	
 			"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
 			"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
 			"sofa", "train", "tvmonitor"]
@@ -72,8 +80,8 @@ class CVControl:
 		self.net.setInput(blob)
 		detections = self.net.forward()
 
-		big_area = 0
-		big_center = 320
+		current_area = 0
+		current_center = 320
 		detected = 0
 		# loop over the detections
 		for i in np.arange(0, detections.shape[2]):
@@ -97,40 +105,61 @@ class CVControl:
 				rect_center = int((startX+endX)/2)
 				rect_area = (endX-startX)*(endY-startY)
 				detected = 1
-				if rect_area > big_area: # big_area and big_center are used so that the TurtleBot always tracks the closest person
-					big_area = rect_area
-					big_center = rect_center
+				if rect_area > current_area:
+					# current_area and current_center are used so that the TurtleBot always tracks the closest person
+					current_area = rect_area
+					current_center = rect_center
 
 	    # if a person is detected, send commands to approach or move away from them depending on proximity
 		if detected:
 			#print(rect_center)
 			#print(rect_area)
-			if big_area > 10000: # Execute if the person is within a reasonable range
+			if current_area > 10000: # Execute if the person is within a reasonable range
+				
+				# call the PID controller to update it and get new speeds
+				[uncliped_lin_speed, uncliped_ang_speed] = self.PID_controller.update([current_area, current_center])
+
+				# clip these speeds to be less then the maximal speed specified above
+				linear_vel  = np.clip(uncliped_lin_speed, -self.max_lin_vel, self.max_lin_vel)
+				angular_vel = np.clip(uncliped_ang_speed, -self.max_ang_vel, self.max_ang_vel)
+				
+
+				"""
 				target_center = 320
 				target_area = 150000
 				# proportional controller for the TurtleBot based on the persons position in the frame and how far they are away
 				kr = .002
-				angular_vel = -kr*(big_center - target_center)
-				maxav = 0.5   # Set a maximum angular velocity for the robot
-				angular_vel = np.max([-maxav, angular_vel])
-				angular_vel = np.min([maxav, angular_vel])
+				angular_vel = -kr*(current_center - target_center)
+				#maxav = 0.5   # Set a maximum angular velocity for the robot
+				#angular_vel = np.max([-maxav, angular_vel])
+				#angular_vel = np.min([maxav, angular_vel])
 
 				kt = 0.0000045
-				linear_vel = -kt*(big_area - target_area)
-				maxv = 0.5   # Set a maximum velocity for the robot
+				linear_vel = -kt*(current_area - target_area)
+				maxv = 0.4   # Set a maximum velocity for the robot
 				linear_vel = np.max([-maxv, linear_vel])
 				linear_vel = np.min([maxv, linear_vel])
 				#print(linear_vel)
+				"""
+
+				if linear_vel < 0.05 and linear_vel > -0.05:
+					linear_vel = 0.0
+				if angular_vel < 0.05 and angular_vel > -0.05:
+					angular_vel = 0.0
+				
+
 				# Send Velocity command to turtlebot
-				self.send_command(linear_vel, angular_vel)	# Publish a motion command to the TurtleBot 
+				self.send_command(linear_vel, angular_vel)	# Publish a motion command to the TurtleBot
 		
 		# Write frames to a video file for up to count_max frames
+		
 		if self.counter < self.count_max:
 			self.out.write(frame)
 			print(self.counter)
 		if self.counter == self.count_max:
 			self.out.release()
 			print('made video')
+		
 		cv2.imshow("Image window", frame)
 		cv2.waitKey(1)
 
@@ -146,11 +175,82 @@ class CVControl:
         # Publish Twist command
         self.cmd_pub.publish(velocity)
 
+
+class PID:
+	'''very simple discrete PID controller'''
+	def __init__(self, target, P, I, D):
+		'''Create a discrete PID controller
+		each of the parameters may be a vector if they have the same length
+
+		Args:
+		target (double) -- the target value(s)
+		P, I, D (double)-- the PID parameter
+
+		'''
+
+		# check if parameter shapes are compatabile.
+		if(not(np.size(P)==np.size(I)==np.size(D)) or ((np.size(target)==1) and np.size(P)!=1) or 
+			(np.size(target)!=1 and (np.size(P) != np.size(target) and (np.size(P) != 1)))):
+			raise TypeError('input parameters shape is not compatable')
+		rospy.loginfo('PID initialised with P:{}, I:{}, D:{}'.format(P,I,D))
+		self.Kp		=np.array(P)
+		self.Ki		=np.array(I)
+		self.Kd		=np.array(D)
+		self.setPoint   =np.array(target)
+
+		self.last_error=0
+		self.integrator = 0
+		self.timeOfLastCall = None
+
+
+	def update(self, current_value):
+		'''Updates the PID controller.
+
+		Args:
+			current_value (double): vector/number of same legth as the target given in the constructor
+
+		Returns:
+			controll signal (double): vector of same length as the target
+
+		'''
+		# current states: [current_area, current_center]
+		current_value=np.array(current_value)
+
+		if(np.size(current_value) != np.size(self.setPoint)):
+			raise TypeError('current_value and target do not have the same shape')
+		if(self.timeOfLastCall is None):
+			# if the PID was called for the first time. we don't know the deltaT yet
+			# no controll signal is applied
+			self.timeOfLastCall = time.clock()
+			return np.zeros(np.size(current_value))
+
+
+		error = self.setPoint - current_value
+		P =  error
+
+		currentTime = time.clock()
+		deltaT      = (currentTime-self.timeOfLastCall)
+
+		# integral of the error is current error * time since last update
+		# self.integrator += error
+		self.integrator += error * deltaT
+		I = self.integrator
+
+		# derivative is difference in error / time since last update
+		# D = error - self.last_error
+		D = (error-self.last_error) / deltaT
+
+		self.last_error = error
+		self.timeOfLastCall = currentTime
+
+		# return control signal
+		return self.Kp*P + self.Ki*I + self.Kd*D
+
+
 def main():
 	ctrl = CVControl()
 	rospy.init_node('image_converter')
 	try:
-		print("getting start")
 		rospy.spin()
 		
 	except KeyboardInterrupt:
